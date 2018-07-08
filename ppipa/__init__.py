8981cbc9f0a62bd297ede90aa8589345c224df58
Peter Pakos <peter.pakos@wandisco.com>
2018-07-08 01:11:02 +0100

    Check address, perform basic bind

--- ppipa/__init__.py
+++ ppipa/__init__.py
@@ -20,4 +20,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from .__version__ import __version__
-from .ppipa import FreeIPA
+from .ppipa import FreeIPAServer
--- ppipa/ppipa.py
+++ ppipa/ppipa.py
@@ -23,8 +23,54 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import absolute_import, print_function
 import logging
 import ldap
+import socket
 
 
-class FreeIPA(object):
-    def __init__(self, ):
+class FreeIPAServer(object):
+    def __init__(self, host, bindpw, binddn='cn=Directory Manager', timeout=5):
         self._log = logging.getLogger(__name__)
+        self._log.debug('Initialising FreeIPA server %s' % host)
+        self._binddn = binddn
+        self._bindpw = bindpw
+        self._timeout = timeout
+
+        try:
+            ip = socket.gethostbyname(host)
+
+            if ip == host:
+                self._ip = ip
+                self._hostname = socket.gethostbyaddr(self._ip)[0]
+            else:
+                self._ip = socket.gethostbyname(host)
+                self._hostname = host
+        except Exception as e:
+            self._log.critical(e)
+            raise
+
+        self._url = 'ldaps://' + self._hostname
+        self._log.debug('URL: %s (IP: %s)' % (self._url, self._ip))
+
+        self._conn = self._get_conn()
+
+    def _get_conn(self):
+        self._log.debug('Setting up LDAP connection')
+        ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+
+        try:
+            conn = ldap.initialize(self._url)
+            conn.set_option(ldap.OPT_NETWORK_TIMEOUT, self._timeout)
+            conn.simple_bind_s(self._binddn, self._bindpw)
+        except (
+            ldap.SERVER_DOWN,
+            ldap.NO_SUCH_OBJECT,
+            ldap.INVALID_CREDENTIALS
+        ) as e:
+            if hasattr(e, 'message') and 'desc' in e.message:
+                msg = e.message['desc']
+            else:
+                msg = e.args[0]['desc']
+            self._log.debug('%s (%s)' % (msg, self._url))
+            return False
+
+        self._log.debug('LDAP connection established')
+        return conn

2926be8e27c4da74bd5eeb79dae62e281afe186a
Peter Pakos <peter.pakos@wandisco.com>
2018-07-10 17:56:24 +0100

    Add support for different types of users

--- ppipa/ppipa.py
+++ ppipa/ppipa.py
@@ -36,16 +36,24 @@ class FreeIPAServer(object):
         self._timeout = timeout
         self._tls = tls
         self._url = 'ldaps://' + host if self._tls else 'ldap://' + host
-        self._conn = self._get_conn()
-        self._fqdn = self._get_fqdn()
+        self._get_conn()
+        self._get_fqdn()
         self._hostname, _, self._domain = str(self._fqdn).partition('.')
-        self._ip = self._get_ip()
+        self._get_ip()
         self._log.debug('Hostname: %s, Domain: %s, IP: %s' % (self._hostname, self._domain, self._ip))
+        self._get_base_dn()
+        self._log.debug('Base DN: %s' % self._base_dn)
+        self._active_user_base = 'cn=users,cn=accounts,' + self._base_dn
+        self._stage_user_base = 'cn=staged users,cn=accounts,cn=provisioning,' + self._base_dn
+        self._preserved_user_base = 'cn=deleted users,cn=accounts,cn=provisioning,' + self._base_dn
+        self._groups_base = 'cn=groups,cn=accounts,' + self._base_dn
+        self._active_users = {}
+        self._stage_users = {}
+        self._preserved_users = {}
 
     def _get_conn(self):
         if self._tls:
             ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-
         try:
             conn = ldap.initialize(self._url)
             conn.set_option(ldap.OPT_NETWORK_TIMEOUT, self._timeout)
@@ -57,9 +65,8 @@ class FreeIPAServer(object):
                 msg = e.args[0]['desc']
             self._log.critical(msg)
             raise
-
         self._log.debug('%s connection established' % ('LDAPS' if self._tls else 'LDAP'))
-        return conn
+        self._conn = conn
 
     @staticmethod
     def _get_ldap_msg(e):
@@ -87,14 +94,70 @@ class FreeIPAServer(object):
             ['nsslapd-localhost'],
             scope=ldap.SCOPE_BASE
         )
-
         if not results and type(results) is not list:
             r = None
         else:
             dn, attrs = results[0]
             r = attrs['nsslapd-localhost'][0].decode('utf-8')
-
-        return r
+        self._fqdn = r
 
     def _get_ip(self):
-        return socket.gethostbyname(self._fqdn)
+        self._ip = socket.gethostbyname(self._fqdn)
+
+    def _get_base_dn(self):
+        results = self._search(
+            'cn=config',
+            '(objectClass=*)',
+            ['nsslapd-defaultnamingcontext'],
+            scope=ldap.SCOPE_BASE
+        )
+        if not results and type(results) is not list:
+            r = None
+        else:
+            dn, attrs = results[0]
+            r = attrs['nsslapd-defaultnamingcontext'][0].decode('utf-8')
+        self._base_dn = r
+
+    @property
+    def active_users(self):
+        if not self._active_users:
+            self._get_users(user_base='active')
+        return self._active_users
+
+    @property
+    def stage_users(self):
+        if not self._stage_users:
+            self._get_users(user_base='stage')
+        return self._stage_users
+
+    @property
+    def preserved_users(self):
+        if not self._preserved_users:
+            self._get_users(user_base='preserved')
+        return self._preserved_users
+
+    def _get_users(self, user_base):
+        results = self._search(
+            getattr(self, '_%s_user_base' % user_base),
+            '(objectClass=*)',
+            ['*'],
+            scope=ldap.SCOPE_ONELEVEL
+        )
+        for dn, attrs in results:
+            uid = attrs['uid'][0].decode('utf-8', 'ignore')
+            a = {}
+            for k, v in attrs.items():
+                if type(v) is list:
+                    a[k] = [i.decode('utf-8', 'ignore') for i in v]
+                else:
+                    a[k] = [v.decode('utf-8')]
+
+            getattr(self, '_%s_users' % user_base)[uid] = a
+
+    def find_uid_by_email(self, email, user_base='active'):
+        uids = []
+        for uid, attrs in getattr(self, '%s_users' % user_base).items():
+            mail = attrs.get('mail')
+            if mail and email in mail:
+                uids.append(uid)
+        return uids
